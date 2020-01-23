e503751ee45ff768f2e66409ec0f45d8be6b1e2f
Bao Zhen <baozhen@xiaoyezi.com>
2020-01-23 21:14:54 +0800

    check vote stake error
    Change-Id: I7ad75d3c9a5229201c109c05218d869a0cf67f02

--- src/test.ts
+++ src/test.ts
@@ -98,3 +98,50 @@ const sleep = (milliseconds:number ) => {
 //     console.log(`unstake ${from} - ${to} - net: ${net_weight} cpu: ${cpu_weight} - ${result}`);
 //   }
 // })();
+
+
+// Check vote stake
+// async function checkVoteStake(check_from: string) {
+//   const client = new EosClient({});
+//   const rexbalRows = await client.getTableRows("eosio", "eosio", "rexbal", {
+//     limit: 100, lower: check_from,
+//   });
+
+//   for (let i = 0; i < rexbalRows.length; i++) {
+//     const rexbal = rexbalRows[i];
+//     if (i == rexbalRows.length - 1) {
+//       return rexbal.owner;
+//     }
+//     const voters = await client.getTableRows("eosio", "eosio", "voters", {
+//       limit: 1, lower: rexbal.owner
+//     });
+//     if (voters.length == 0 || voters[0].owner !== rexbal.owner || voters[0].staked <= 0) {
+//       continue;
+//     }
+//     const voter = voters[0];
+
+//     const delegateRows = await client.getTableRows("eosio", voter.owner, "delband", {
+//       limit: 200
+//     });
+//     const delegate = delegateRows.reduce((total, currentValue, currentIndex, arr)=>
+//       {
+//         return total + Math.floor(
+//           parseFloat(currentValue.net_weight.split(' ')[0]) * 10000 +
+//           parseFloat(currentValue.cpu_weight.split(' ')[0]) * 10000);
+//       }, 0);
+
+//     if (delegate > voter.staked) {
+//       console.log(`checkVoteStake: ${voter.owner} voter.staked=${voter.staked} < net+cpu=${delegate}`);
+//     }
+//   }
+//   console.log("all voters searched!!!");
+//   return check_from;
+// }
+
+// (async () => {
+//   let check_from = ".";
+//   for (let i = 0; i < 10000; i++) {
+//     console.log(`${i}: start to search from ${check_from}`);
+//     check_from = await checkVoteStake(check_from);
+//   }
+// })();
